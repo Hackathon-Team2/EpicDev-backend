ae864d3d0947a2f39747ede30ef735f0813cf526
unknown <maxime.ugur@gmail.com>
2021-09-24 23:47:16 +0200

    Modification basepackage swagger

--- src/main/java/org/hackathon/apis/Configs/SwaggerConfig.java
+++ src/main/java/org/hackathon/apis/Configs/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("EpicDev.BackEnd.WebApi"))
+                .apis(RequestHandlerSelectors.basePackage("org.hackathon.apis"))
                 .paths(PathSelectors.any())
                 .build();
     }
